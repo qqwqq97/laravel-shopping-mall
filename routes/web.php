3d733b7dcd8440452f96160dd18270ffd72f4415
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-10-14 15:00:18 +0900

    component準備

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\componentTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/component-test1', [componentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [componentTestController::class, 'showComponent2']);
 
 require __DIR__.'/auth.php';
