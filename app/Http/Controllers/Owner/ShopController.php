8de3be9bf9375170b5183dc9da2db8fb04e8e178
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-10-29 16:17:33 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UploadImageRequest;
 use InterventionImage;
 
 class ShopController extends Controller
@@ -43,7 +44,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid() ){
