2a7c2288f396a1e13103850321b56657c8434b9e
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-10-31 19:53:17 +0900

    image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,8 +32,10 @@ Route::prefix('shops')->
     Route::get('index', [ShopController::class, 'index'])->name('shops.index');
     Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
-
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
